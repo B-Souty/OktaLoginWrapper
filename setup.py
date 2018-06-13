83bc8f9547356a23519def5b901b712f0d6c9f73
B-Souty <benjamin.souty@gmail.com>
2018-06-13 12:01:53 +0200

    Modify setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='oktaloginwrapper',
       install_requires=['lxml', 'requests'],
       version='0.1',
       description='Okta login made easy from the command line without API token',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='https://github.com/B-Souty/OktaLoginWrapper',
       author='B-Souty',
       author_email='benjamin.souty@gmail.com',
       license='MIT',
       packages=['oktaloginwrapper'],
-      zip_safe=False)
+      classifiers=(
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+      ),)
